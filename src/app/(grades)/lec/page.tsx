0aa63e25f690dd4d60af807bdf420e555d36c5ce
‪zeyad ashraf‬‏ <84470860+zeyadashraf2002@users.noreply.github.com>
2025-10-05 21:09:12 +0300

    Delete

--- src/app/(grades)/lec/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function lecPage() {
-    return (
-        <div className="p-6 text-center">
-            <h1 className="text-3xl font-bold">Lec Page</h1>
-            <p className="text-gray-600 mt-2 text-lg">This is a Lec page</p>
-        </div>
-    );
-}
